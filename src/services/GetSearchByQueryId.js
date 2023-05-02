f3aa8a684ba594e0b0083a090839c83ab363be8a
The Maximalist <hello@themaximalist.com>
2023-05-02 10:32:05 -0700

    cleanup console.log

--- src/services/GetSearchByQueryId.js
+++ src/services/GetSearchByQueryId.js
@@ -2,7 +2,6 @@ const Search = require("../models/search");
 const Query = require("../models/query");
 
 module.exports = async function GetSearchByQueryId(query_id) {
-    console.log(query_id);
     if (!query_id) throw new Error('No query_id provided');
 
     const query = await Query.findByPk(query_id, {
@@ -10,8 +9,6 @@ module.exports = async function GetSearchByQueryId(query_id) {
             model: Search,
         }]
     });
-
-    console.log(query);
 
     if (!query) throw new Error('No query found');
 
