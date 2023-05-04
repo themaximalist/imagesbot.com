ee8a74eabba911031a4275d8f3abdeb3c2173068
The Maximalist <hello@themaximalist.com>
2023-05-04 12:37:38 -0700

    got share page dynamic grid working

--- src/services/GenerateConcept.js
+++ src/services/GenerateConcept.js
@@ -1,15 +1,26 @@
 const log = require("debug")("imagesbot:services:generate");
 
-const { Concept, Result } = require("../models");
+const { Query, Concept, Result } = require("../models");
 const CreateConcept = require("../services/CreateConcept");
 const { randomElement } = require("../utils");
 const GetSearchByQueryId = require("./GetSearchByQueryId");
 
 module.exports = async function GenerateConcept(query_id) {
 
-    const search = await GetSearchByQueryId(query_id);
+    const query = await Query.findByPk(query_id);
+    if (!query) throw new Error(`query ${query_id} not found`);
 
-    // STEP 1: Check favorite concepts across all queries
+    // STEP 1: Check specific concepts for this query
+    const concepts = await Concept.findAll({ where: { QueryId: query_id } });
+    if (concepts.length == 0) { // either new search or a new query..let's generate new concepts
+        log(`creating a new concept for new query ${query_id}`);
+        return await CreateConcept(query_id);
+    }
+
+    const search = await GetSearchByQueryId(query_id);
+    if (!search) throw new Error(`search ${query_id} not found`);
+
+    // STEP 2: Check favorite concepts across all queries
     const favorites = await Concept.findAll({
         where: { SearchId: search.id },
         include: [{ model: Result, required: true, where: { favorite: true } }]
@@ -21,15 +32,14 @@ module.exports = async function GenerateConcept(query_id) {
         return await CreateConcept(query_id, favoriteConcepts);
     }
 
-    // STEP 2: Check specific concepts for this query
-    const concepts = await Concept.findAll({ where: { QueryId: query_id } });
 
+    // STEP 3: If we have more than 3 concepts, use a random one
     if (concepts.length > 3) {
         log(`using random concept for ${query_id}`);
         return randomElement(concepts);
     }
 
-    // STEP 3: Create a new concept
+    // STEP 4: Create a new concept
     log(`creating a new concept for ${query_id}`);
     return await CreateConcept(query_id);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
             }
         },
     },
+    safelist: [
+        "grid-cols-1",
+        "grid-cols-2",
+        "grid-cols-3",
+        "grid-cols-4",
+        "grid-cols-5",
+    ],
     plugins: [],
 }
 
